fdc4e3da05a3dffddee9823ba687504a7f90c9b3
Adam Presley <adam@adampresley.com>
2021-04-18 15:51:36 -0500

    Forgot the interface

--- interfaces.go
+++ interfaces.go
@@ -12,4 +12,5 @@ type Conn interface {
 	Exec(ctx context.Context, sql string, arguments ...interface{}) (pgconn.CommandTag, error)
 	Ping(ctx context.Context) error
 	Query(ctx context.Context, sql string, args ...interface{}) (pgx.Rows, error)
+	QueryRow(ctx context.Context, sql string, args ...interface{}) pgx.Row
 }
